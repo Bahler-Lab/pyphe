e0ba3ac39839afae43ecbce54cee9db8d7064ef5
Stephan Kamrad <yeastbeast@github.com>
2019-11-26 15:39:57 +0000

    fixed script list

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='pyphe',
-      version='0.9',
+      version='0.91.20191126',
       description='Python toolbox for phenotype analysis of arrayed microbial colonies',
       url='https://github.com/Bahler-Lab/pyphe',
       author='Stephan Kamrad',
       author_email='stephan.kamrad@crick.ac.uk',
       license='MIT',
       packages=['pyphe'],
-      scripts=['bin/pyphe-scan', 'bin/pyphe-scan-timecourse', 'bin/pyphe-growthcurves', 'bin/pyphe-analysis', 'bin/pyphe-quantify'],
+      scripts=['bin/pyphe-scan', 'bin/pyphe-scan-timecourse', 'bin/pyphe-growthcurves', 'bin/pyphe-analyse', 'bin/pyphe-quantify', 'bin/pyphe-analyse-gui'],
       install_requires=[
           'pandas',
           'matplotlib',

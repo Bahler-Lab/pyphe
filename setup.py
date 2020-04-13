26dd28ec5668345522511f481637aa7f4cf51b10
yeastbeast <stephan.kamrad.15@ucl.ac.uk>
2020-04-13 22:28:25 +0100

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
     
 setup(name='pyphe',
-      version='0.94.20200329',
+      version='0.95',
       description='Python toolbox for phenotype analysis of arrayed microbial colonies',
       long_description=long_description,
       long_description_content_type="text/markdown",
